07f9a589b09af02efdd0d46462e566cdfd54a220
Thiago Araújo <thiagomvaraujo@gmail.com>
2017-09-22 15:54:21 -0300

    Append conversion pixel

--- js/script.js
+++ js/script.js
@@ -43,6 +43,7 @@
 function joinWhitelist() {
     openNewTab("https://auctus.us16.list-manage2.com/subscribe?u=e6c2bc48f91f09fab4afe5bee&id=63a6a00141");
     registerClickEvent('JoinWhitelist');
+    appendConversionPixel();
 }
 
 function downloadWhitepaper(){
@@ -61,4 +62,15 @@ function registerClickEvent(category) {
         eventCategory: category,
         eventAction: 'click'
     });
+}
+
+function appendConversionPixel() {
+    if (typeof twttr !== 'undefined') twttr.conversion.trackPid('ny69l', { tw_sale_amount: 0, tw_order_quantity: 0 });
+    if (typeof twttr !== 'undefined') twttr.conversion.trackPid('ny637', { tw_sale_amount: 0, tw_order_quantity: 0 });
+    var i = new Image(); i.src = "https://alb.reddit.com/t.gif?q=CAAHAAABAAoACQAAAAAA6DArAA==&s=WZRd1JXnZRV1jlvXS-XxOhVvuUCEppJaeyGI1Xak6fs=";
+    $('body').append('<img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=ny69l&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />');
+    $('body').append('<img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=ny69l&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />');
+    $('body').append('<img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=ny637&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />');
+    $('body').append('<img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=ny637&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />');
+    $('body').append('<img height="1" width="1" style="display:none" src="https://alb.reddit.com/t.gif?q=CAAHAAABAAoACQAAAAAA6DArAA==&s=WZRd1JXnZRV1jlvXS-XxOhVvuUCEppJaeyGI1Xak6fs=" />');    
 }
