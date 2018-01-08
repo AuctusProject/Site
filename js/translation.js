da5435048760a3e96bdeb2000f1d1b96d25f3b90
Daniel Silveira <dansilveira@gmail.com>
2018-01-08 01:39:45 -0200

    translations

--- js/translation.js
+++ js/translation.js
@@ -1,15 +1,12 @@
-(function ($) {
-    changeLanguage("pt-br");
-})(jQuery);
+
 
 function changeLanguage(language) {
     $("#translation").remove();
     var s = document.createElement("script");
     s.type = "text/javascript";
-    s.src = "translation/" + language + ".json";
+    s.src = "../translation/" + language + ".json";
     s.id = "translation";
     $("head").append(s);
-    debugger;
     for (var section in i18nData) {
         sectionData = i18nData[section]
         for (var key in sectionData) {
