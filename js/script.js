8ebfff252995702acb9c4a1ac5ece93995a4a218
Vinicius Melo <viniciusfbm@gmail.com>
2018-02-06 13:21:19 -0200

    Token sale page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
     var interval = setInterval(onTimer, 1000);
 
     function onTimer() {
-        var date = Date.UTC(2017, 10, 14, 14, 00);
+        var date = Date.UTC(2018, 2, 27, 14, 00);
         
         var element = $('.timer');
         var diff = date - new Date().getTime();
