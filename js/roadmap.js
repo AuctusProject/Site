b57b43095fb67ed2d9816ea4457320bad35c1d95
danielduartef <danielduartefigueiredo@gmail.com>
2018-11-05 18:47:39 -0200

    roadmap arrow click

--- js/roadmap.js
+++ js/roadmap.js
@@ -2,3 +2,17 @@ $('.year').click(function (e) {
     $('.highlighted').removeClass('highlighted');
     $(this).addClass('highlighted');
 });
+
+$('.arrow').click(function (e) {
+    var yearToHighlight = null;
+    if ($(this).hasClass('invert-horizontally')) {
+        yearToHighlight = $('.highlighted').prev('.year');
+    }
+    else {
+        yearToHighlight = $('.highlighted').next('.year');
+    }
+    if (yearToHighlight.length > 0) {
+        $('.highlighted').removeClass('highlighted');
+        yearToHighlight.addClass('highlighted');
+    }
+});
