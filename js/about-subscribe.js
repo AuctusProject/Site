2c83c272c724d5f632eadcf4803639f721fd4079
danielduartef <danielduartefigueiredo@gmail.com>
2018-11-08 17:22:28 -0200

    newsletter subscription

---
+++ js/about-subscribe.js
@@ -0,0 +1,14 @@
+$('#btn-subscription').click(function () {
+    var name = $('#name').val();
+    var email = $('#email').val();
+
+    //TODO call api
+    //https://auctusplatformapidev.azurewebsites.net/api/v1/website/emails
+    //POST
+    /*
+    {
+        "email": email,
+	    "name": name
+    }
+    */
+})
