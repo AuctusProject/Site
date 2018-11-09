5024543624400efe1fa89a7172eb5bf807750ab5
danielduartef <danielduartefigueiredo@gmail.com>
2018-11-09 19:43:49 -0200

    email and name validation

--- js/about-subscribe.js
+++ js/about-subscribe.js
@@ -2,6 +2,26 @@ $('#btn-subscription').click(function () {
     var inputName = $('#name').val();
     var inputEmail = $('#email').val();
 
+    if (!inputName)
+        $('#name').addClass('invalid');
+    else
+        $('#name').removeClass('invalid');
+    if (!inputEmail || !validateEmail(inputEmail))
+        $('#email').addClass('invalid');
+    else
+        $('#email').removeClass('invalid');
+
+    if (inputName && inputEmail && validateEmail(inputEmail)) {
+        subscribeToNewsletter(inputName, inputEmail);
+    }
+})
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function subscribeToNewsletter(inputName, inputEmail) {
     $.ajax({
         type: 'POST',
         url: 'https://auctusplatformapidev.azurewebsites.net/api/v1/website/emails',
@@ -15,4 +35,4 @@ $('#btn-subscription').click(function () {
             alert('Great success');
         }
     });
-})
+}
